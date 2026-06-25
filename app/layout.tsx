8d21a77c24105c73be3245aa1eee3132b0fad790
spencergoergen <spencer.goergen@gmail.com>
2026-06-25 11:57:37 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const ibmPlexSans = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: 'Agentic AI in Wealth Management | IBM watsonx',
+  title: 'Agentic AI in Wealth Management | powered by IBM watsonx',
   description:
     'Leveraging Agentic AI, we built a user-friendly chat interface that drives productivity for financial advisors by helping them better prioritize, research, and prepare for client meetings.',
   keywords: 'IBM watsonx, Agentic AI, Wealth Management, Financial Advisor, AI Assistant',
